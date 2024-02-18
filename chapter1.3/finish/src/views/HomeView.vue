341baab7814f9cca4211c46d8b1dbf7ef1dd73f1
M-S <mariam.shene@gmail.com>
2024-02-18 17:07:39 +0100

    add chapter 1.3 code example

---
+++ chapter1.3/finish/src/views/HomeView.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { Ref } from "vue";
+
+const bookBtnContent = "Book a flight";
+const captions = ref<Array<string>>([
+  "Soar for less",
+  "Ready to takeoff",
+  "Let your dreams take flight",
+  "Escape to your dreams"
+])
+const getTitleContent = function (): string {
+  return captions.value[captions.value.length - 1];
+}
+let title: Ref<string> = ref(getTitleContent());
+let intervalId = setInterval(() => {
+  title.value = getTitleContent();
+  captions.value.pop();
+  console.log("title", title.value);
+  console.log("captions", captions.value);
+}, 1000)
+
+setTimeout(() => {
+  clearInterval(intervalId)
+}, 4000);
+
+</script>
+
+<template>
+  <main>
+    <section class="hero is-fullheight">
+      <div class="hero-head">
+        <div class="container">
+          <figure class="image is-64x64">
+            <img src="../assets/logo.png" alt="Logo" />
+          </figure>
+        </div>
+      </div>
+
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title is-1">{{ title }}</h1>
+          <button class="button is-link is-medium">{{ bookBtnContent }}</button>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+<style scoped>
+.hero {
+  background-image: url(https://images.unsplash.com/photo-1586500036065-bdaeac7a4feb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3088&q=80);
+  background-position: center;
+  background-size: cover;
+}
+
+.image {
+  margin: 16px;
+}
+</style>
---
+++ chapter1.3/start/src/views/HomeView.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+
+const bookBtnContent = "Book a flight";
+const captions = [
+  "Soar for less",
+  "Ready to takeoff",
+  "Let your dreams take flight",
+  "Escape to your dreams"
+]
+const getTitleContent = function (): string {
+  const captionIndex = Math.floor(Math.random() * captions.length);
+  return captions[captionIndex];
+}
+
+let title: string = getTitleContent();
+let intervalId = setInterval(() => {
+  title = getTitleContent();
+  console.log("title", title);
+}, 1000)
+
+setTimeout(() => {
+  clearInterval(intervalId)
+}, 10000);
+
+</script>
+
+<template>
+  <main>
+    <section class="hero is-fullheight">
+      <div class="hero-head">
+        <div class="container">
+          <figure class="image is-64x64">
+            <img src="../assets/logo.png" alt="Logo" />
+          </figure>
+        </div>
+      </div>
+
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <h1 class="title is-1">{{ title }}</h1>
+          <button class="button is-link is-medium">{{ bookBtnContent }}</button>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+<style scoped>
+.hero {
+  background-image: url(https://images.unsplash.com/photo-1586500036065-bdaeac7a4feb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3088&q=80);
+  background-position: center;
+  background-size: cover;
+}
+
+.image {
+  margin: 16px;
+}
+</style>
